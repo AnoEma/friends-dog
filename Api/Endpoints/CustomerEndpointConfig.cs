f0e73559084e79350b372c5a938d7622deb0d674
AnoEma <anoemangi@gmail.com>
2022-12-21 16:04:23 -0300

    Add Endpoints

---
+++ Api/Endpoints/CustomerEndpointConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Endpoints;
+
+public static class CustomerEndpointConfig
+{
+    public static void AddEndpoints(WebApplication app)
+    {
+        app.MapGet("customer", () =>
+        {
+            return "ok";
+        });
+    }
+}
