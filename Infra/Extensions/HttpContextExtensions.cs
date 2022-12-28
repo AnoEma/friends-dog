ed1cca662a4cbd533f51bc9f79e21d308bc15ce5
AnoEma <anoemangi@gmail.com>
2022-12-28 14:17:22 -0300

    Add HttpContextExtensions

---
+++ Infra/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Infra.Extensions;
+
+public static class HttpContextExtensions
+{
+    public static string GetFormValue(this HttpContext httpContext, string key)
+    {
+        if (string.IsNullOrEmpty(key)
+            || httpContext == null
+            || httpContext.Request == null
+            || httpContext.Request.Form == null)
+        {
+            return string.Empty;
+        }
+
+        var form = httpContext.Request.Form;
+
+        if (!form.Any() || !httpContext.Request.Form.ContainsKey(key))
+        {
+            return string.Empty;
+        }
+
+        return httpContext.Request.Form[key];
+    }
+}
