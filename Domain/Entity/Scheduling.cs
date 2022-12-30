b9ff1cbbfe840584f53b304ac81ae9cb1b237b11
AnoEma <anoemangi@gmail.com>
2022-12-30 14:32:28 -0300

    Create CTO Scheduling

--- Domain/Entity/Scheduling.cs
+++ Domain/Entity/Scheduling.cs
@@ -2,6 +2,22 @@
 
 public class Scheduling
 {
+    public Scheduling(
+        DateTime appointment,
+        string outsideaddress,
+        string entryaddress,
+        string zipcodeoutsideaddress,
+        string zipcodeentryaddress,
+        float distance)
+    {
+        this.appointment = appointment;
+        this.outsideaddress = outsideaddress;
+        this.entryaddress = entryaddress;
+        this.zipcodeoutsideaddress = zipcodeoutsideaddress;
+        this.zipcodeentryaddress = zipcodeentryaddress;
+        this.distance = distance;
+    }
+
     public DateTime appointment { get; set; }
     public string outsideaddress { get; set; }
     public string entryaddress { get; set; }
