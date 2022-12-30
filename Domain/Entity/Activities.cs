3214af95669d28d19c730bbc92c5fc49dea92e6e
AnoEma <anoemangi@gmail.com>
2022-12-30 14:11:16 -0300

    Add object in the class Activities

--- Domain/Entity/Activities.cs
+++ Domain/Entity/Activities.cs
@@ -1,6 +1,28 @@
-﻿namespace Domain.Entity
+﻿namespace Domain.Entity;
+
+public class Activities
 {
-    public class Activities
+    public Activities(
+        string name,
+        string description,
+        Types types,
+        DateTime started,
+        DateTime lastUpdated,
+        bool active)
     {
+        Name = name;
+        Description = description;
+        Types = types;
+        Started = started;
+        LastUpdated = lastUpdated;
+        Active = active;
     }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public Types Types { get; set; }
+    public DateTime Started { get; set; }
+    public DateTime LastUpdated { get; set; }
+    public bool Active { get; set; }
 }
---
+++ Domain/Entity/Types.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entity;
+
+public enum Types
+{
+    Game = 1,
+    Show = 2,
+    Party = 3
+}
